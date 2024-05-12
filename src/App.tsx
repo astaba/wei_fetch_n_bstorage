67e880ce638875317fff2e07bf4532a4cf285252
Astar <bahouidi99@gmail.com>
2024-05-12 15:59:48 +0100

    Reset playground for browser storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" className="anchor">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" className="anchor">
-          <img src={reactLogo} className="logo hover:drop-shadow-[0_0_2em_rgba(97,218,251,0.667)] motion-safe:animate-[spin_20s_linear_infinite]" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-[3.2em] leading-[1.1]">Vite + React</h1>
-      <div className="p-[2em]">
-        <button onClick={() => setCount((count) => count + 1)} className="btn-focus cursor-pointer rounded-lg border border-solid border-transparent bg-gray-100 px-[1.2em] py-[0.6em] font-sans text-[1em] font-medium transition-[border] duration-[0.25s] hover:border-[#646cff] focus:outline focus:outline-2 focus:outline-slate-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-slate-400 dark:bg-gray-900">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div>
+      <h1 className="text-[3.2em] leading-[1.1] text-center">
+        Browser Storage
+      </h1>
+      <button
+        onClick={handleClick}
+        className="btn-focus cursor-pointer rounded-lg border border-solid border-transparent bg-gray-100 px-[1.2em] py-[0.6em] font-sans text-[1em] font-medium tracking-widest transition-[border] duration-[0.25s] hover:border-[#646cff] focus:outline focus:outline-2 focus:outline-slate-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-slate-400 dark:bg-gray-900"
+      >
+        Toggle
+      </button>
+      <hr />
+      {isOpen && (
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic iure
+          ullam quam quas explicabo. Minima veniam quasi quidem numquam iusto
+          praesentium totam consectetur, dolore laboriosam alias. Esse doloribus
+          modi eius.
         </p>
-      </div>
-      <p className="text-gray-400">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+    </div>
   );
 }
 
