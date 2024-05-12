7d1bac677fb01214775489c7a6759c4367d1c13b
Astar <bahouidi99@gmail.com>
2024-05-13 00:06:39 +0100

    Trigger useEffect programmatically

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import useLocalStorage from "./hook/useLocalStorage";
+
 interface IHit {
   title: string;
   objectID: number;
@@ -11,18 +13,39 @@ const API_ENDPOINT = "http://hn.algolia.com/api/v1/search?query=";
 
 function App() {
   const [hits, setHits] = useState<IHit[]>([]);
+  const [searchTerm, setSearchTerm] = useLocalStorage<string>(
+    "hitSearch",
+    "landscape",
+  );
+  const [query, setQuery] = useState(searchTerm);
+
+  const handleBtnClick = () => {
+    setQuery(searchTerm);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get(`${API_ENDPOINT}landscape`);
+      if (!query) return;
+      const result = await axios.get(`${API_ENDPOINT}${query}`);
       // console.log(result);
       setHits(result.data.hits);
     };
     fetchData();
-  }, []);
+  }, [query]);
   return (
     <div>
       <h1 className="text-[3.2em] leading-[1.1] text-center">Fetch Data</h1>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="text-inherit text-lg px-2 py-1 mr-4"
+        />
+        <button type="button" onClick={handleBtnClick} className="btn">
+          Search
+        </button>
+      </div>
       <hr />
       {hits.length > 0 && (
         <ul>
